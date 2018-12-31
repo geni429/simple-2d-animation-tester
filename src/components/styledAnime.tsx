bf89cdfa5b6248d4af9f8d87ccbf1ab2b926b0c8
Roy Jeong <roy.jeong@sendbird.com>
2018-12-31 10:55:56 +0900

    [Update] styled-anime, () => {} to function keyword

--- src/components/styledAnime.tsx
+++ src/components/styledAnime.tsx
@@ -2,60 +2,72 @@ import * as React from "react";
 import styled, { SimpleInterpolation, css } from "styled-components";
 import anime from "animejs";
 
-export const styledAnime = (
+export const styledAnime = function styledAnime(
   TargetComponent: keyof JSX.IntrinsicElements | React.ComponentType<any>
-) => (
-  style: TemplateStringsArray | NonNullable<SimpleInterpolation>,
-  ...interpolations: SimpleInterpolation[]
-) => (config: StyledAnimeParams) => {
-  const StyledComponent = styled(TargetComponent)`
-    ${css(style, ...interpolations)};
-  `;
+) {
+  return function(
+    style: TemplateStringsArray | NonNullable<SimpleInterpolation>,
+    ...interpolations: SimpleInterpolation[]
+  ) {
+    return function(config: StyledAnimeParams) {
+      const StyledComponent = styled(TargetComponent)`
+        ${css(style, ...interpolations)};
+      `;
 
-  return class extends React.PureComponent<StyledAnimeComponentProps> {
-    private targetRef = React.createRef<HTMLElement>();
-    private animation: anime.AnimeInstance | undefined = void 0;
+      return class extends React.PureComponent<StyledAnimeComponentProps> {
+        private targetRef = React.createRef<HTMLElement>();
+        private animation: anime.AnimeInstance | undefined = void 0;
 
-    private isPlayed = (): boolean => {
-      const { played } = this.props;
+        public constructor(props: StyledAnimeComponentProps) {
+          super(props);
 
-      return played !== undefined ? played : true;
+          this.isPlayed = this.isPlayed.bind(this);
+          this.play = this.play.bind(this);
+          this.pause = this.pause.bind(this);
+        }
+
+        private isPlayed(): boolean {
+          const { played } = this.props;
+
+          return played !== void 0 ? played : true;
+        }
+
+        private play(): void {
+          this.animation && this.animation.play();
+        }
+
+        private pause(): void {
+          this.animation && this.animation.pause();
+        }
+
+        public componentDidMount() {
+          const props = { ...this.props };
+
+          delete props.children;
+          delete props.played;
+
+          this.animation = anime({
+            targets: this.targetRef.current,
+            autoplay: false,
+            ...config,
+            ...props
+          });
+
+          this.isPlayed() ? this.play() : this.pause();
+        }
+
+        public render() {
+          if (this.animation !== void 0) {
+            this.isPlayed() ? this.play() : this.pause();
+          }
+
+          return (
+            <StyledComponent ref={this.targetRef}>
+              {this.props.children}
+            </StyledComponent>
+          );
+        }
+      };
     };
-
-    private play = () => {
-      this.animation && this.animation.play();
-    };
-
-    private pause = () => {
-      this.animation && this.animation.pause();
-    };
-
-    public componentDidMount() {
-      const props = { ...this.props };
-
-      delete props.children;
-      delete props.played;
-
-      this.animation = anime({
-        targets: this.targetRef.current,
-        autoplay: false,
-        ...config,
-        ...props
-      });
-
-      this.isPlayed() ? this.play() : this.pause();
-    }
-
-    public render() {
-      if (this.animation !== void 0) {
-        this.isPlayed() ? this.play() : this.pause();
-      }
-
-      return (
-        <StyledComponent ref={this.targetRef}>
-          {this.props.children}
-        </StyledComponent>
-      );
-    }
   };
 };
